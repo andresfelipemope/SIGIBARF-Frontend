28ebfc5dc7c01f73e6709e11093b4ef70d86d1d5
Andres Monsalve <andresfelipemope@ufps.edu.co>
2026-04-25 16:30:39 -0500

    FIX: modulo de navbar completo

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { ShoppingCart, User } from 'lucide-react';
 
 export function Navbar() {
   const router = useRouter();
@@ -9,28 +10,40 @@ export function Navbar() {
     catalogo: true,
     calculadora: false,
     nosotros: false,
+    gestion: false,
   });
+
+  const [admin, setAdmin] = useState(false);
 
   const handleClick = (section, route) => {
     setActive({
       catalogo: false,
       calculadora: false,
       nosotros: false,
+      gestion: false,
       [section]: true,
     });
 
     //route.push(route);
   };
 
+  const handleUser = () => {
+    route.push("/login")
+  }
+
+  const handleCart = () => {
+    route.push("/cart")
+  }
+
   return (
-    <div className="flex justify-between p-3 items-center">
+    <div className="flex justify-between p-3 items-center border-b-2 border-green-600">
 
       <div className="flex gap-2 items-center font-bold">
-        <img src="/images/AthleticBarf.png" alt="logo" className="w-12 h-12" />
+        <img src="/images/AthleticBarf.png" alt="logo" className="w-16 h-16" />
         <p className="text-2xl">ATHLETIC BARF</p>
       </div>
 
-      <div className="flex gap-6">
+      <div className="flex gap-6 text-lg">
         <p className={`cursor-pointer
         ${active.catalogo
             ? "border-b-2 border-green-500 text-green-700 transition-all duration-300"
@@ -55,10 +68,21 @@ export function Navbar() {
           onClick={() => handleClick("nosotros", "/nosotros")}>
           Nosotros
         </p>
+        {admin && (<p className={`cursor-pointer
+        ${active.gestion
+            ? "border-b-2 order-green-500 text-green-700 transition-all duration-300"
+            : "hover:border-b-2 border-gray-200 transition-all duration-300"
+          }`}
+          onClick={() => handleClick("gestion", "/gestion")}>
+          Gestion de Inventario
+        </p>)}
 
       </div>
 
-      <div>logos</div>
+      <div className="flex gap-4">
+        <User className="cursor-pointer" onClick={() => handleUser()} />
+        <ShoppingCart className="cursor-pointer" onClick={() => handleCart()} />
+      </div>
     </div>
   );
 }
